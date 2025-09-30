d2655e27fc0e417474394682384b1fe16c237881
RobertHielkema <r.j.hielkema.1@student.rug.nl>
2025-09-30 19:22:50 +0200

    fixed infinite quarantine

--- person.py
+++ person.py
@@ -6,23 +6,26 @@ class Person:
     def __init__(self, name, app: App_controller):
         self.name = name
         self.infection_status = 'Susceptible'
+        self.total_days = 0
         self.days_exposed = 0
         self.days_infected = 0
         self.quarantined = False
+        self.days_quarantined = 0
         self.careless = False
         self.app = app
     
 
     def timestep(self):
+        self.total_days += 1
         if self.infection_status == 'Susceptible':  # nothing happens if healthy
             pass
-        elif self.infection_status == 'Exposed':    # after 6 days of being infected, become sick
+        elif self.infection_status == 'Exposed':    # after 6 days of being infected, become exposed
             self.days_exposed += 1
             p = math.exp(2 * (self.days_exposed - 3.2))   # exponential growth, ~100% by day 4 as per paper
             if random.random() < p:
                 self.infection_status = 'Infected'
                 self.days_exposed = 0
-        elif self.infection_status == 'Infected':   # after 6 days of being sick, become healthy
+        elif self.infection_status == 'Infected':   # after 6 days of being exposed, become removed
             self.days_infected += 1
             p = math.exp(2 * (self.days_infected - 3.2))       # same exponential form
             if random.random() < p:
@@ -30,12 +33,13 @@ class Person:
                 self.days_infected = 0
                 if self.app:
                     self.app.trigger_quarantine(self)
-
-            # TODO: When to quarantine? Now its immediate upon infection but maybe should be after a day or two?
-            self.quarantined = True
+                self.quarantined = False  # after removed, no longer quarantined
+        if self.quarantined:
+            self.days_quarantined += 1
+            if self.days_quarantined >= 8: # after 8 days, someone would not be Exposed or Infected anymore
+                self.quarantined = False
+                self.days_quarantined = 0
             
-        elif self.infection_status == 'Removed':
-            self.quarantined = False  # after removed, no longer quarantined
 
 
     def infect(self):
